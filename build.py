db65b5d75d05af2b11002c45fb299893f38181fe
Per Kraulis <per.kraulis@gmail.com>
2020-09-18 21:59:06 +0200

    Added requirements.

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 "Build the 'docs' contents from the latest CSV file from Goodreads."
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import csv
 import os
@@ -12,16 +12,17 @@ GOODREADS_PATH = os.path.expanduser('~/Dropbox/archive/goodreads')
 DOCS_PATH = os.path.join(os.getcwd(), 'docs')
 TEMPLATES_PATH = os.path.join(os.getcwd(), 'templates')
 
+# Source file: latest CSV dump from Goodreads
 filenames = sorted(os.listdir(GOODREADS_PATH))
 filepath = os.path.join(GOODREADS_PATH, filenames[-1])
-print('reading', filepath)
+print('source:', filepath)
 
 with open(filepath) as infile:
     reader = csv.reader(infile)
     header = next(reader)
     rows = list(reader)
 
-print(len(rows), 'books')
+print(len(rows), 'books found')
 
 bookshelves_col = None
 for pos, item in enumerate(header):
@@ -43,13 +44,15 @@ exbookshelf = set()
 for row in rows:
     bookshelves.update([s.strip() for s in row[bookshelves_col].split(',')])
     exbookshelf.update([s.strip() for s in row[exbookshelf_col].split(',')])
-tags = list(bookshelves.difference(exbookshelf))
+tags = sorted(bookshelves.difference(exbookshelf))
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(TEMPLATES_PATH),
     autoescape=jinja2.select_autoescape(['html'])
 )
 
-with open(os.path.join(DOCS_PATH, 'index.html'), 'w') as outfile:
+path = os.path.join(DOCS_PATH, 'index.html')
+with open(path, 'w') as outfile:
     template = env.get_template('index.html')
     outfile.write(template.render(tags=tags))
+    print('wrote', path)
